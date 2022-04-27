b03305b488eda3c8178089ef98776f609e010736
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-04-27 19:12:22 -0400

    Update createReleaseFiles.ps1

--- createReleaseFiles.ps1
+++ createReleaseFiles.ps1
@@ -1,9 +1,9 @@
 #generates zips and pbos for release
 #zips
-Compress-Archive -LiteralPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_5ManSquads.VR" -DestinationPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\5ManTemplates\TAS_MissionTemplate_5ManSquads.VR.zip"
-Compress-Archive -LiteralPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_8ManSquads.VR" -DestinationPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\5ManTemplates\TAS_MissionTemplate_8ManSquads.VR.zip"
-Compress-Archive -LiteralPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_10ManSquads.VR" -DestinationPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\5ManTemplates\TAS_MissionTemplate_10ManSquads.VR.zip"
-#pbos
-MakePbo [-A] "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_5ManSquads.VR"
-MakePbo [-A] "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_8ManSquads.VR"
-MakePbo [-A] "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_10ManSquads.VR"
+Compress-Archive -LiteralPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_5ManSquads.VR" -DestinationPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\Release_Files\TAS_MissionTemplate_5ManSquads.VR.zip"
+Compress-Archive -LiteralPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_8ManSquads.VR" -DestinationPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\Release_Files\TAS_MissionTemplate_8ManSquads.VR.zip"
+Compress-Archive -LiteralPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_10ManSquads.VR" -DestinationPath "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\Release_Files\TAS_MissionTemplate_10ManSquads.VR.zip"
+#pbos #CURRENTLY DON'T WORK, something wrong with path specification
+MakePbo [-A] "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_5ManSquads.VR" "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\Release_Files\TAS_MissionTemplate_5ManSquads.VR.pbo"
+MakePbo [-A] "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_8ManSquads.VR" "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\Release_Files\TAS_MissionTemplate_8ManSquads.VR.pbo"
+MakePbo [-A] "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\TAS_MissionTemplate_10ManSquads.VR" "C:\Users\andre\Documents\Arma 3 - Other Profiles\Guac\mpmissions\Github\TAS-Mission-Template\Release_Files\TAS_MissionTemplate_10ManSquads.VR.pbo"
